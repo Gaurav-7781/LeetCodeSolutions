94dc52f8c26ae2c70ef6afa146a653466a655535
Gaurav <gauravsingh13333@gmail.com>
2026-04-21 15:24:15 +0530

    Smallest Even Multiple

---
+++ 2413.cpp
@@ -0,0 +1,20 @@
+// Intuition
+// using brain, we have find lcm
+
+// Approach
+// just use the built in function
+
+// Complexity
+// Time complexity:
+// O(1)
+
+// Space complexity:
+// O(1)
+
+// Code
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        return lcm(2,n);
+    }
+};
