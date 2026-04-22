8b6f54e3c85a1d1f6050a184883330431f08fb1d
Gaurav <gauravsingh13333@gmail.com>
2026-04-22 15:18:30 +0530

    Add Two Integers

---
+++ 2235.cpp
@@ -0,0 +1,20 @@
+// Intuition
+// just add two element
+
+// Approach
+// you can add easily just use + sign
+
+// Complexity
+// Time complexity:
+// O(1)
+
+// Space complexity:
+// O(1)
+
+// Code
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1+num2;
+    }
+};
