5bb834927d703863e1011b6b4fc71258af8a11c0
Gaurav <gauravsingh13333@gmail.com>
2026-06-17 17:55:00 +0530

    Baseball Game

---
+++ 682.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        vector<int> ans;
+
+        for(string op : operations){
+            if(op=="C"){
+                ans.pop_back();
+            }else if(op=="D"){
+                ans.push_back(2*ans.back());
+            }else if(op=="+"){
+                int n=ans.size();
+                ans.push_back(ans[n-1]+ans[n-2]);
+            }else{
+                ans.push_back(stoi(op));
+            }
+        }
+        int sum=0;
+
+        for(int x : ans){
+            sum+=x;
+        }
+        return sum;
+    }
+};
